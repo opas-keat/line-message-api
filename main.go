0c2c9235ce113a038c39423cea108847b18ecdbf
opas-keat <opas.keat@gmail.com>
2020-11-24 11:35:48 +0700

    add recover

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	// Fiber instance
 	app := fiber.New()
 
+	// Middleware
+	app.Use(recover.New())
+
 	// Routes
 	app.Get("/", hello)
+
+	app.Post("/webhook", webhook)
+
+	// 404 Handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
 
 	// Start server
 	log.Fatal(app.Listen(":3000"))
 }
 
-// Handler
+// Handler webhook
+func webhook(c *fiber.Ctx) error {
+	return c.SendString("Hi👋!, This Line Message Api")
+}
+
+// Handler hello
 func hello(c *fiber.Ctx) error {
-	return c.SendString("Hi5, World 👋!")
+	return c.SendString("Hi👋!, This Line Message Api")
 }
